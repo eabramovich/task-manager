373bc5c018afa9ca648f6dcf666038758cf67060
Евгения Абрамович <ewg.abramovich2014@yandex.ru>
2024-11-10 19:03:34 +0300

    Update TaskForm

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../UI/Button/Button";
 import FormInput from "../UI/FormInput/FormInput";
+import IconButton from "../UI/IconButton/IconButton";
 import "./TaskForm.scss";
 
 const TaskForm = ({ task }) => {
@@ -22,6 +23,51 @@ const TaskForm = ({ task }) => {
           <div className="task-form__fields-container">
             <div className="task-form__field-wrapper">
               <FormInput label="Тема" required={true} value={task.title} />
+            </div>
+            <div className="task-form__field-wrapper">
+              <FormInput label="Статус" required={true} value={task.status} />
+            </div>
+            <div className="task-form__field-wrapper">
+              <FormInput label="Описание" required={true} value={task.description} />
+            </div>
+            <div className="task-form__field-wrapper">
+              <div className="task-form__input-wrapper">
+              <FormInput label="Продукт" required={true} value={task.product} />
+              </div>
+              <div className="task-form__icon-button-wrapper">
+                <IconButton icon="add" />
+              </div>
+            </div>
+            <div className="task-form__field-wrapper">
+              <FormInput label="Рабочие заметки" required={true} value={task.notes} />
+            </div>
+            <div className="task-form__field-wrapper">
+              <FormInput label="Приоритет" required={true} value={task.priority} />
+            </div>
+            <div className="task-form__field-wrapper">
+              <div className="task-form__input-wrapper">
+                <FormInput label="Ответственный" required={true} />
+              </div>
+              <div className="task-form__icon-button-wrapper">
+                <IconButton icon="add" />
+              </div>
+              <div className="task-form__icon-button-wrapper">
+                <IconButton icon="search" />
+              </div>
+            </div>
+            <div className="task-form__field-wrapper">
+              <div className="task-form__input-wrapper">
+                <FormInput label="Группа" required={true} />
+              </div>
+              <div className="task-form__icon-button-wrapper">
+                <IconButton icon="add" />
+              </div>
+              <div className="task-form__icon-button-wrapper">
+                <IconButton icon="search" />
+              </div>
+            </div>
+            <div className="task-form__field-wrapper task-form__field-wrapper_full">
+              <FormInput label="Комментарии" required={true} value={task.comments} />
             </div>
           </div>
         </div>
